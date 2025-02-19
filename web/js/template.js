2ff69ec2d6a7a0f45d6f6346112c4c1920a7b4fb
alm0st <alan.roy43@outlook.com>
2025-02-18 22:17:24 -0500

    Update header

--- web/js/template.js
+++ web/js/template.js
@@ -6,18 +6,19 @@ class webheader extends HTMLElement {
         document.head.appendChild(link);
         this.innerHTML = `
             <header>
-                <button style="all:unset; width: 18%; cursor:pointer;" onclick="window.location.href='index.html'">    
-                    <img src="images/logo.png" style="width:100%;height:auto;">
+                <div class="img-box">
+                <button onclick="window.location.href='index.html'" style="all:unset;">
+                    <img src="images/logo.png" class="logo">
                 </button>
+                </div>
                 <div class="headersection">
-                    <button class="headerbutton" onclick="window.location.href='help/index.html'">Help</button>
-                    <button class="headerbutton" onclick="window.location.href='account/auth/signup/index.html'">Signup</button>
-                    <button class="headerbutton" onclick="window.location.href='account/auth/login/login.html'">Login</button>
+                    <button class="headerbutton" id="helpbutton" onclick="window.location.href='help/index.html'">Help</button>
+                    <button class="headerbutton" id="signupbutton" onclick="window.location.href='account/auth/signup/index.html'">Signup</button>
+                    <button class="headerbutton" id="loginbutton" onclick="window.location.href='account/auth/login/login.html'">Login</button>
                     <button class="headerbutton" id="accountbutton" onclick="window.location.href='account/banking/index.html'">Account</button>
-                    <button onclick="alert('The search feature is currently unavailable.')" style="all:unset; width: 10%; cursor:pointer; ">
-                        <img src="images/search.png" style="background-color:transparent; width: 60%; height: auto;">
+                    <button class="search-btn" onclick="alert('The search feature is currently unavailable.')">
+                        <img src="images/search.png" class="search-icon">
                     </button>
-                    <!--<div style="width:5%"></div>-->
                 </div>
             </header>
         `;
@@ -40,6 +41,23 @@ class webheader extends HTMLElement {
         else {
             accountButton.style.display = 'none';
         }
+
+        
+        const title = document.title;
+        
+        var activeButton;
+
+        if (title == "Help"){
+            activeButton = this.querySelector('#helpbutton');
+        }
+        else if (title == "Signup"){
+            activeButton = this.querySelector('#signupbutton');
+        }
+        else if (title == "Login"){
+            activeButton = this.querySelector('#loginbutton');
+        }
+
+        activeButton.className = "headerbutton active";
     }
 }
 
